da22aecbd52288cb526682ff61b781c083098a4c
Vlad Zubin <zoobeen1@gmail.com>
2023-10-16 18:04:54 +0300

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { useSearchParams } from 'react-router-dom';
+// import { useSearchParams } from 'react-router-dom';
 
 export const Movies = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  console.log('All params - ', searchParams);
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // console.log('All params - ', searchParams);
   return <div>Movies</div>;
 };
